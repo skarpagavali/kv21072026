faa56c2b4ebfc044e7cf44b3bb3f874f114d1dd2
Karpagavalli S <karpagavalli.10819897@ltimindtree.com>
2026-07-21 21:42:27 +0530

    test project

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        String expectedTitle = "Google";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)) {
+            System.out.println("Test Case Passed");
+
+        } else {
+
+            System.out.println("Test Case Failed");
+
+        }
+
+        driver.quit();
+
+    }
+}
